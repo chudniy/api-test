4cce310f631205d7ac013149ab77fca5f1f7a614
Yevhen Cudnyi <yevhen.chudnyi@ekreative.com>
2019-08-11 20:07:36 +0300

    Create API controller

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -9,13 +9,16 @@
 namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+
 
 /**
  * User
  *
  * @ORM\Table("users")
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
  */
 class User extends BaseUser
 {
@@ -37,5 +40,84 @@ class User extends BaseUser
     public function getId()
     {
         return $this->id;
+    }
+    
+    /**
+     * @ORM\OneToMany(targetEntity="Invitation", mappedBy="sender")
+     */
+    private $sentInvitations;
+    
+    /**
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Invitation", mappedBy="recipient")
+     */
+    private $receivedInvitations;
+    
+    public function __construct()
+    {
+        parent::__construct ();
+        $this->sentInvitations = new ArrayCollection();
+        $this->receivedInvitations = new ArrayCollection();
+    }
+    
+    /**
+     * @return Collection|Invitation[]
+     */
+    public function getSentInvitations(): Collection
+    {
+        return $this->sentInvitations;
+    }
+    
+    public function addSentInvitation(Invitation $sentInvitation): self
+    {
+        if (!$this->sentInvitations->contains($sentInvitation)) {
+            $this->sentInvitations[] = $sentInvitation;
+            $sentInvitation->setSender($this);
+        }
+        
+        return $this;
+    }
+    
+    public function removeSentInvitation(Invitation $sentInvitation): self
+    {
+        if ($this->sentInvitations->contains($sentInvitation)) {
+            $this->sentInvitations->removeElement($sentInvitation);
+            // set the owning side to null (unless already changed)
+            if ($sentInvitation->getSender() === $this) {
+                $sentInvitation->setSender(null);
+            }
+        }
+        
+        return $this;
+    }
+    
+    /**
+     * @return Collection|Invitation[]
+     */
+    public function getReceivedInvitations(): Collection
+    {
+        return $this->receivedInvitations;
+    }
+    
+    public function addReceivedInvitation(Invitation $receivedInvitation): self
+    {
+        if (!$this->receivedInvitations->contains($receivedInvitation)) {
+            $this->receivedInvitations[] = $receivedInvitation;
+            $receivedInvitation->setRecipient($this);
+        }
+        
+        return $this;
+    }
+    
+    public function removeReceivedInvitation(Invitation $receivedInvitation): self
+    {
+        if ($this->receivedInvitations->contains($receivedInvitation)) {
+            $this->receivedInvitations->removeElement($receivedInvitation);
+            // set the owning side to null (unless already changed)
+            if ($receivedInvitation->getRecipient() === $this) {
+                $receivedInvitation->setRecipient(null);
+            }
+        }
+        
+        return $this;
     }
 }
---
+++ src/AppBundle/Repository/InvitationRepository.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace AppBundle\Repository;
+
+use AppBundle\Entity\Invitation;
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * @method Invitation|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Invitation|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Invitation[]    findAll()
+ * @method Invitation[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class InvitationRepository extends EntityRepository
+{
+    // /**
+    //  * @return Invite[] Returns an array of Invite objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('i')
+            ->andWhere('i.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('i.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Invite
+    {
+        return $this->createQueryBuilder('i')
+            ->andWhere('i.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
