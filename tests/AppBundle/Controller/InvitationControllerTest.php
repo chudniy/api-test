c6e1d777c881fa531baa7e6b76d0d7169021dcfd
Yevhen Chudnyi <yevhen.chudnyi@ekreative.com>
2019-09-05 17:40:41 +0300

    Add some tests

---
+++ tests/AppBundle/Controller/InvitationControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use PHPUnit\Framework\TestCase;
+
+class InvitationControllerTest extends TestCase
+{
+    public function testIndex()
+    {
+        $this->assertTrue(false);
+    }
+}
